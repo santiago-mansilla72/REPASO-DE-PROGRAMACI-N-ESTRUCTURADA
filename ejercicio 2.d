26775c719c6063972683f31e913e07bfc100539f
santiago-mansilla72 <santiago.mansilla.172@alu.tecnica29de6.edu.ar>
2025-05-19 08:48:48 -0300

    Create ejercicio 2.d

---
+++ ejercicio 2.d
@@ -0,0 +1,6 @@
+#Escribe un programa que pida al usuario que ingrese una palabra y luego muestre cada letra de la palabra en una línea separada.
+
+palabra= input("ingresa una palabra: ")
+
+for letra in palabra:
+    print(letra)
